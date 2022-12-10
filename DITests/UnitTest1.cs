5f21e1b2424cedcc79fcfe9b564bd1678eb66eaa
bobroshaa <bobroshaa@mail.ru>
2022-12-10 11:35:37 +0300

    add some tests

--- DITests/UnitTest1.cs
+++ DITests/UnitTest1.cs
@@ -10,103 +10,123 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void DefaultRegistrationTest()
     {
         var dependencies = new DependenciesConfiguration();
         dependencies.Register<IService1, Service1>();
         dependencies.Register<AbstractService2, Service2>();
- 
+
         var provider = new DependencyProvider(dependencies);
-        var service1 = provider.Resolve<IService1>();
-        Assert.That(service1.GetType().ToString(), Is.EqualTo("DITests.Service1"));
+        var service = provider.Resolve<IService1>();
+        Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.Service1"));
     }
-    
+
     [Test]
-    public void Test2()
+    public void AbstractRegistrationTest()
+    {
+        var dependencies = new DependenciesConfiguration();
+        dependencies.Register<AbstractService2, Service2>();
+
+        var provider = new DependencyProvider(dependencies);
+        var service = provider.Resolve<AbstractService2>();
+        Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.Service2"));
+    }
+
+
+    [Test]
+    public void RegistrationAsSelfTest()
+    {
+        var dependencies = new DependenciesConfiguration();
+        dependencies.Register<Service1, Service1>();
+
+        var provider = new DependencyProvider(dependencies);
+        var service = provider.Resolve<Service1>();
+        Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.Service1"));
+    }
+
+    [Test]
+    public void RecursiveRegistrationTest()
     {
         var dependencies = new DependenciesConfiguration();
         dependencies.Register<IService, ServiceImpl>();
         dependencies.Register<IRepository, RepositoryImpl>();
- 
+
         var provider = new DependencyProvider(dependencies);
-        
-        var service1 = provider.Resolve<IService>();
-        Assert.That(service1.GetType().ToString(), Is.EqualTo("DITests.ServiceImpl"));
+        var service = provider.Resolve<IService>();
+        Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.ServiceImpl"));
     }
 
     [Test]
-    public void Test3()
+    public void SingletonRegistrationTest()
     {
         var dependencies = new DependenciesConfiguration();
         dependencies.Register<IService1, Service1>(LivingTime.Singleton);
- 
+
         var provider = new DependencyProvider(dependencies);
-        var service1 = provider.Resolve<IService1>();
-        service1.SomeData = "singleton";
-        var newService1 = provider.Resolve<IService1>();
-        Assert.That(newService1.SomeData, Is.EqualTo("singleton"));
+        var service = provider.Resolve<IService1>();
+        service.SomeData = "singleton";
+        var newService = provider.Resolve<IService1>();
+        Assert.That(newService.SomeData, Is.EqualTo("singleton"));
     }
-    
+
     [Test]
-    public void Test4()
+    public void ManyImplementationTest()
     {
         var dependencies = new DependenciesConfiguration();
-
         dependencies.Register<IService1, Service1>();
         dependencies.Register<IService1, Service3>();
+        
         var provider = new DependencyProvider(dependencies);
         IEnumerable<IService1> services = provider.Resolve<IEnumerable<IService1>>();
         Assert.That(services.Count(), Is.EqualTo(2));
     }
-    
-    interface IService5<T>
+
+    interface IService5<TRepository> where TRepository : IRepository
     {
-            
     }
 
-    class ServiceImpl5<T> : IService5<T>
+    class ServiceImpl5<TRepository> : IService5<TRepository>
+        where TRepository : IRepository
     {
-        public ServiceImpl5(T repository)
+        public ServiceImpl5(TRepository repository)
         {
-               
         }
     }
-    
+
     [Test]
-    public void Test5()
+    public void GenericRegistrationTest()
     {
         var dependencies = new DependenciesConfiguration();
-
         dependencies.Register<IRepository, RepositoryImpl>();
         dependencies.Register<IService5<IRepository>, ServiceImpl5<IRepository>>();
+        
         var provider = new DependencyProvider(dependencies);
         var service = provider.Resolve<IService5<IRepository>>();
         Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.Tests+ServiceImpl5`1[DITests.IRepository]"));
     }
-    
+
     [Test]
-    public void Test6()
+    public void OpenGenericTest()
     {
         var dependencies = new DependenciesConfiguration();
-
         dependencies.Register(typeof(IService5<>), typeof(ServiceImpl5<>));
         dependencies.Register<IRepository, RepositoryImpl>();
+        
         var provider = new DependencyProvider(dependencies);
         var service = provider.Resolve<IService5<IRepository>>();
         Assert.That(service.GetType().ToString(), Is.EqualTo("DITests.Tests+ServiceImpl5`1[DITests.IRepository]"));
     }
-    
+
     enum ServiceImplementations
     {
         First,
         Second
     }
-    
+
     [Test]
-    public void Test7()
+    public void EnumTest()
     {
         var dependencies = new DependenciesConfiguration();
-        
         dependencies.Register<IService1, Service1>(LivingTime.InstancePerDependency, ServiceImplementations.First);
         dependencies.Register<IService1, Service3>(LivingTime.InstancePerDependency, ServiceImplementations.Second);
 
